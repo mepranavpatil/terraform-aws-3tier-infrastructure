1e6ac5dbc90223c09387fd74e3eb6615d73de0dd
mepranavpatil <mepranavpatil@gmail.com>
2026-08-21 14:23:34 +0530

    refactor: add EC2 instance resources for web servers with user data configuration

--- ec2.tf
+++ ec2.tf
@@ -8,3 +8,37 @@ data "aws_ami" "amazon_linux" {
     values = ["al2023-ami-*-x86_64"]
   }
 }
+
+resource "aws_instance" "web1" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.public-a.id
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2_sg.id
+  ]
+
+  user_data = file("${path.module}/userdata.sh")
+
+  tags = {
+    Name = "web-server-1"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.public-b.id
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2_sg.id
+  ]
+
+  user_data = file("${path.module}/userdata.sh")
+
+  tags = {
+    Name = "web-server-2"
+  }
+}
