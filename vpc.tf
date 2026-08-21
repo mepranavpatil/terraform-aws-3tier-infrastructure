2c6cf7387324b7daf1eb19620432d43c76c98670
mepranavpatil <mepranavpatil@gmail.com>
2026-08-21 14:35:54 +0530

    fix: correct subnet ID references in route table associations

--- vpc.tf
+++ vpc.tf
@@ -74,22 +74,22 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "public_a" {
-  subnet_id      = aws_subnet.public-a.id
+  subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public.id
 }
   
 resource "aws_route_table_association" "public_b" {
-  subnet_id      = aws_subnet.public-b.id
+  subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private_a" {
-  subnet_id      = aws_subnet.private-a.id
+  subnet_id      = aws_subnet.private_a.id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private_b" {
-  subnet_id      = aws_subnet.private-b.id
+  subnet_id      = aws_subnet.private_b.id
   route_table_id = aws_route_table.private.id
 }
 
