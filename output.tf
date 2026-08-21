1c080a952e3b04e84862b39871bc369f3a7fe6b0
mepranavpatil <mepranavpatil@gmail.com>
2026-08-21 14:11:49 +0530

    add outputs for ALB, EC2, and RDS security group IDs in Terraform configuration

--- output.tf
+++ output.tf
@@ -33,3 +33,14 @@ output "public_route_table_id" {
 output "private_route_table_id" {
   value = aws_route_table.private.id
 }
+output "alb_security_group_id" {
+  value = aws_security_group.alb_sg.id
+}
+
+output "ec2_security_group_id" {
+  value = aws_security_group.ec2_sg.id
+}
+
+output "rds_security_group_id" {
+  value = aws_security_group.rds_sg.id
+}
