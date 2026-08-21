d1c6953e0cd1032cb8cac181540e05001807e7a3
mepranavpatil <mepranavpatil@gmail.com>
2026-08-21 14:10:50 +0530

    add RDS security group with MySQL ingress rules from EC2

--- security-groups.tf
+++ security-groups.tf
@@ -75,3 +75,33 @@ resource "aws_security_group" "ec2_sg" {
     Name = "ec2-sg"
   }
 }
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-security-group"
+  description = "RDS Security Group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "MySQL from EC2"
+
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+
+    security_groups = [
+      aws_security_group.ec2_sg.id
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
+}
